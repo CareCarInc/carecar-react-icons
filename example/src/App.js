c7934ad3a3f304ce500bc220869570969ea2bba8
Brandon Orther <brandon@uptrend.tech>
2018-10-27 00:45:16 -0700

    Initial working SVGR icons

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from '@carecar/react-icons'
+import icons from '@carecar/react-icons'
+
+console.log(icons.items)
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <icons.AddIcon />
+
+        {Object.entries(icons).map(([name, Icon]) => (
+          <div key={Icon.displayName}>
+          <h2>{Icon.displayName || console.log({name,Icon})}</h2>
+            <div><Icon color="red" viewBox="0 0 4em 4em" /></div>
+
+
+          </div>
+        ))}
+        {/* <ExampleComponent text='Modern React component module' /> */}
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,77 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+// import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
+// export default class ExampleComponent extends Component {
+//   static propTypes = {
+//     text: PropTypes.string
+//   }
 
-  render() {
-    const {
-      text
-    } = this.props
+//   render() {
+//     const {
+//       text
+//     } = this.props
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+//     return (
+//       <div className={styles.test}>
+//         Example Component: {text}
+//       </div>
+//     )
+//   }
+// }
+
+// import { ReactComponent as ReactIcon } from './react-icon.svg'
+
+import { ReactComponent as AddCompanion } from './icons/add-companion.svg'
+import { ReactComponent as AddIcon } from './icons/add-icon.svg'
+import { ReactComponent as AppointmentDate } from './icons/appointment-date.svg'
+import { ReactComponent as AppointmentTime } from './icons/appointment-time.svg'
+import { ReactComponent as Companion } from './icons/companion.svg'
+import { ReactComponent as DilterOptions } from './icons/dilter-options.svg'
+import { ReactComponent as Driver } from './icons/driver.svg'
+import { ReactComponent as DropoffLocation } from './icons/dropoff-location.svg'
+import { ReactComponent as Edit } from './icons/edit.svg'
+import { ReactComponent as Language } from './icons/language.svg'
+import { ReactComponent as Members } from './icons/members.svg'
+import { ReactComponent as MenuThreeDots } from './icons/menu-three-dots.svg'
+import { ReactComponent as Mobile } from './icons/mobile.svg'
+import { ReactComponent as Notes } from './icons/notes.svg'
+import { ReactComponent as Notifications } from './icons/notifications.svg'
+import { ReactComponent as Phone } from './icons/phone.svg'
+import { ReactComponent as PickupLocation } from './icons/pickup-location.svg'
+import { ReactComponent as Reporting } from './icons/reporting.svg'
+import { ReactComponent as SelectionFilter } from './icons/selection-filter.svg'
+import { ReactComponent as TripLink } from './icons/trip-link.svg'
+import { ReactComponent as Trips } from './icons/trips.svg'
+import { ReactComponent as TypeOfVisit } from './icons/type-of-visit.svg'
+
+// const CareCarIcon = ({icon, ...props}) => {
+
+//   componentName
+// }
+
+export default {
+  AddCompanion,
+  AddIcon,
+  AppointmentDate,
+  AppointmentTime,
+  Companion,
+  DilterOptions,
+  Driver,
+  DropoffLocation,
+  Edit,
+  Language,
+  Members,
+  MenuThreeDots,
+  Mobile,
+  Notes,
+  Notifications,
+  Phone,
+  PickupLocation,
+  Reporting,
+  SelectionFilter,
+  TripLink,
+  Trips,
+  TypeOfVisit
 }
